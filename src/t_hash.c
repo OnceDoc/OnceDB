8a5da3751120605e2e90a79a54d02ec96f7008a8
DXJ3X1 <zihua_xue@yahoo.com>
2016-12-28 15:37:31 +0800

    Add function hmselectCommand in t_hash.c

--- src/t_hash.c
+++ src/t_hash.c
@@ -843,3 +843,66 @@ void hselectCommand(client *c) {
     dictReleaseIterator(di);
     setDeferredMultiBulkLength(c,replylen,numkeys);
 }
+
+void hmselectCommand(client *c) {
+    if ((c->argc % 2) == 1) {
+        addReplyError(c,"wrong number of arguments for HMSELECT");
+        return;
+    }
+
+    dictIterator *di;
+    dictEntry *de;
+    sds pattern = c->argv[1]->ptr, hpattern;
+    int i, plen = sdslen(pattern), allkeys, hplen, anyhval;
+    unsigned long numkeys = 0;
+    void *replylen = addDeferredMultiBulkLength(c);
+
+    di = dictGetSafeIterator(c->db->dict);
+    allkeys = (pattern[0] == '*' && pattern[1] == '\0');
+    while((de = dictNext(di)) != NULL) {
+        sds key = dictGetKey(de);
+        robj *keyobj;
+
+        if (allkeys || stringmatchlen(pattern,plen,key,sdslen(key),0)) {
+            keyobj = createStringObject(key,sdslen(key));
+            if (expireIfNeeded(c->db,keyobj) == 0) {
+                robj *valobj = dictGetVal(de);
+                if (valobj->type == OBJ_HASH) {
+                    for (i = 2; ; i += 2) {
+                        if (hashTypeExists(valobj,c->argv[i])) {
+                            robj *hvalobj;
+                            hvalobj = hashTypeGetObject(valobj, c->argv[i]);
+                            if (hvalobj != NULL) {
+                                sds hval = hvalobj->ptr;
+                                hpattern = c->argv[i+1]->ptr;
+                                hplen = sdslen(hpattern);
+                                anyhval = (hpattern[0] == '*' && hpattern[1] == '\0');
+                                if (anyhval || stringmatchlen(hpattern,hplen,hval,sdslen(hval),0)) {
+                                    if (i + 2 >= c->argc) {
+                                        addReplyBulk(c,keyobj);
+                                        numkeys++;
+                                        decrRefCount(hvalobj);
+                                        break;
+                                    }
+                                    decrRefCount(hvalobj);
+                                }
+                                else {
+                                    break;
+                                    decrRefCount(hvalobj);
+                                }
+                            }
+                            else {
+                                break;
+                                decrRefCount(hvalobj);
+                            }
+                        }
+                        else break;
+                    }
+                }
+            }
+            decrRefCount(keyobj);
+        }
+    }
+    dictReleaseIterator(di);
+    setDeferredMultiBulkLength(c,replylen,numkeys);
+}
